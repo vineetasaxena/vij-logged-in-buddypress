9a6e11a1ccdda17e5a9d6521cffa9531f949bdfa
Vijaita Narain <vineetasaxena@yahoo.com>
2016-08-27 19:24:06 -0400

    all changes

---
+++ includes/class-vij-logged-in-buddypress-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://websitepro.website
+ * @since      1.0.0
+ *
+ * @package    Vij_Logged_In_Buddypress
+ * @subpackage Vij_Logged_In_Buddypress/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Vij_Logged_In_Buddypress
+ * @subpackage Vij_Logged_In_Buddypress/includes
+ * @author     Vijaita Narain <vineetasaxena@yahoo.com>
+ */
+class Vij_Logged_In_Buddypress_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
